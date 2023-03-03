feeccbbd929af3af64de2727a5d6fbd60277027f
Omar Rezk <omar.rizk6@gmail.com>
2023-03-02 21:36:04 -0500

    finished the video description section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import Nav from './components/Nav/Nav'
 import Video from './components/Video/Video'
+import videoData from '../src/data/video-details.json'
+import data from '../src/data/videos.json'
+import { useState } from "react"
 
-function App() {
+export default function App() {
+
+  const [defaultVideo, setDefaultVideo] = useState(videoData[0]);
+  function selectVideo(id) {
+    const selectedVideo = videoData.find((video) => {
+      return id === video.id;
+    });
+    setDefaultVideo(selectedVideo);
+  }
+
   return (
     <>
     <Nav/>
-    <Video/>
+    <Video
+    videosList={data}
+    activeVideo={defaultVideo}
+    onClickVavItem={setDefaultVideo}/>
     </>
   );
 }
 
-export default App;
+
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import videoData from '../../data/video-details.json'
 import './Video.scss'
+import viewsIcon from '../../assets/icons/views.svg'
+import likesIcon from '../../assets/icons/likes.svg'
 
-const Video = () => {
-    const videoBanner = videoData[0]
+
+function stringDate(dateInput) {
+    const date = new Date(dateInput);
+    const dateString = date.toLocaleDateString();
+    return dateString;
+}
+
+const Video = (props) => {
+
     return (
         <>
             <div className="banner">
-                <video className="banner__video controls" controls width="250" poster={videoBanner.image}></video>
+                <video className="banner__video controls" controls width="250" poster={props.activeVideo.image}></video>
+            </div>
+            <div className="video__container">
+                <h1 className="video__container-title">{props.activeVideo.title}</h1>
+                <div className="video__container-box">
+                    <div className="video__container-boxInfo">
+                        <p className="video__container-name">By {props.activeVideo.channel}</p>
+                        <p className="video__container-date">{stringDate(props.activeVideo.timestamp)}</p>
+                    </div>
+                    <div className="video__container-boxInfo">
+                       <p className="video__container-watch"><img src={viewsIcon} className="video__container-image" alt="" />{props.activeVideo.views}</p>
+                       <p className="video__container-like"><img src={likesIcon} className="video__container-image" alt="" />{props.activeVideo.likes}</p>
+                    </div>
+                </div>
+                <div className="video__container-description">
+                    <p className="video__container-summary">{props.activeVideo.description}</p>
+                </div>
             </div>
         </>
     );
