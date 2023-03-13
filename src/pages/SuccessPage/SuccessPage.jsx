e24c0ce52fd2222e93331870e098049104793b5b
Omar Rezk <omar.rizk6@gmail.com>
2023-03-12 21:22:46 -0400

    finalized styling on the success page

--- src/pages/SuccessPage/SuccessPage.jsx
+++ src/pages/SuccessPage/SuccessPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom"
-import './SuccessPage'
+import '../SuccessPage/SuccessPage.scss'
 
 const SuccessPage = () => {
     return (
         <div className="successPage">
-            <h1>Success Page</h1>
+            <h1 className="successPage__header">Success Page</h1>
+                <p className="successPage__title">Click here to go to the Homepage</p>
             <Link to="/">
-            <button className="successPage__button">Homepage</button>
+            <button className="successPage__button">
+               <p className="successPage__text">Homepage</p></button>
             </Link>
         </div>
     );
