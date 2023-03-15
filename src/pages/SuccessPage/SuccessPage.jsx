06fd56a63ec80c218125cdfce6d33db4580b1623
Omar Rezk <omar.rizk6@gmail.com>
2023-03-15 15:33:51 -0400

    finished editing success page

--- src/pages/SuccessPage/SuccessPage.jsx
+++ src/pages/SuccessPage/SuccessPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom"
 import '../SuccessPage/SuccessPage.scss'
+import HappyPikachu from '../../assets/images/happy-Pokemon-Pikachu.png'
 
 const SuccessPage = () => {
     return (
         <div className="successPage">
-            <h1 className="successPage__header">Success Page</h1>
-                <p className="successPage__title">Click below to go to the Homepage</p>
-            <Link to="/">
+            <h1 className="successPage__header">Upload Successful 🎉</h1>
+                <img className="successPage__image" src={HappyPikachu} alt="happy-pikachu" />
+                <p className="successPage__subheader">View the uploaded video on the homepage by clicking below</p>
+            <Link className='successPage__link' to="/">
                 <button className="successPage__button">
-                    <p className="successPage__text">Click Here!</p>
+                    <p>Click Here!</p>
                </button>
             </Link>
         </div>
